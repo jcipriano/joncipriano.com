c13bdc504ace3c44ba229e5d45cabbd5d030beea
Jonathan Cipriano <cipriano.jonathan@gmail.com>
2014-03-15 14:42:21 -0700

    Cube animation

--- dist/js/frontend.js
+++ dist/js/frontend.js
@@ -2325,14 +2325,14 @@ $(function () {
 (function(){
 
   var vis = {},
-  $container, container, camera, scene, renderer, width, height, cubes = [];
+  $container, container, camera, scene, renderer, width, height, cubeHolder, cubes = [], cubeTotal;
 
   vis.init = function() {
     $container = $('#vis-container');
     container = $container[0];
 
-    camera = new THREE.PerspectiveCamera( 20, $container.width() / $container.height(), 1, 10000 );
-    camera.position.z = 2000;
+    camera = new THREE.PerspectiveCamera( 50, $container.width() / $container.height(), 1, 10000 );
+    camera.position.z = 900;
 
     scene = new THREE.Scene();
 
@@ -2340,64 +2340,65 @@ $(function () {
     renderer.setSize($container.width(), $container.height());
     $container .append(renderer.domElement);
 
-    var light1 = new THREE.SpotLight( 0xFFFFFF, 1.5 );
-    light1.position.set( 0, 0, 5000 );
+    var light1, light2;
+
+    light1 = new THREE.SpotLight( 0xFFFFFF, 0.9 );
+    light1.position.set( -5000, 0, 3000 );
     scene.add( light1 );
 
-    var material = new THREE.MeshLambertMaterial( { color: 0xCCCCCC } ),
-    i = 0, len = 40,
-    xRange = $container.width() * 2,
-    yRange = $container.height() * 3,
-    xPos = xRange, yPos, scale,
-    cube,
-    size;
+    light2 = new THREE.SpotLight( 0xFFFFFF, 0.9 );
+    light2.position.set( 5000, 0, 3000 );
+    scene.add( light2 );
 
-    for(i; i<len; i++) {
-      xPos = xPos - 120;
-      yPos = Math.random() * yRange;
-      scale = xPos / xRange;
-      size =  100 * scale + 20;
-      cube = new THREE.Mesh(new THREE.CubeGeometry(400 * Math.random() + 100, 10, size), material);
+    var material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } ),
+    c = 0, cols = 20,
+    r = 0, rows = 4,
+    cube;
+    
+    cubeHolder = new THREE.Object3D();
+    cubeHolder.position.x = -2000;
+    cubeHolder.position.y = -200;
+    cubeHolder.rotation.x = 0.1;
+    scene.add(cubeHolder);
 
+    for(r; r<rows; r++) {
 
-      cube.position.x = xPos - xRange / 2;
-      cube.position.y = yPos - yRange / 2;
-      cube.position.z = (Math.random() * -3000) + 1500;
+      for(c; c<cols; c++) {
+        cube = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 5), material);
+        cube.position.x = (c * 201); 
+        cube.position.y = (r * 201);
+        cube.position.z = Math.random() * -300 + 100;
 
+        cubeHolder.add(cube);
+        cubes.push(cube);
+      }
 
-      cube.rotation.x = 200;
-      //cube.rotation.y = Math.random() * 100;
-
-      scene.add(cube);
-      cubes.push(cube);
+      c = 0;
+      cols = 20;
     }
+
+    cubeTotal = cubes.length;
 
     vis.onResize()
     vis.animate();
+
+    vis.tween();
 
     $(window).resize(function() {
       vis.onResize();
     });
   };
 
-  vis.animate = function() {
-
-    var i = 39, cube;
+  vis.tween = function () {
+    var i = cubeTotal-1, cube;
     for(i; i > -1; i--) {
       cube = cubes[i]; 
-
-      if(cube.position.x > width * 2) {
-        cube.position.x = -(width * 2)
-      }
-
-      cube.position.x += 2;
-
-      //cube.rotation.x += 0.01;
-      //cube.rotation.y += 0.01;
+      new cubeTween(cube);
     }
+  };
 
-    //requestAnimationFrame( vis.animate );
-
+  vis.animate = function() {
+    requestAnimationFrame( vis.animate );
     renderer.render(scene, camera);
   };
 
@@ -2409,6 +2410,21 @@ $(function () {
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
   };
+
+
+  var cubeTween = function (cube) {
+    this.cube = cube;
+
+    this.tween = function () {
+      var that = this;
+      TweenLite.to(this.cube.position, 1 + Math.random()*4, {z: Math.random() * -300 + 100, delay: Math.random() * 2 + 2, onComplete: function() {
+        that.tween(cube);
+      }});
+    }
+
+    this.tween();
+  };
+
 
   $(function(){
     vis.init();
